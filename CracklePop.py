f6ae2602786b90f8fea90c98bab4378062d558f3
Sammy Jolene <sammyshorey@gmail.com>
2022-11-21 11:27:16 -0500

    Updated code

--- CracklePop.py
+++ CracklePop.py
@@ -1,11 +1,9 @@
 for number in range(101):
    if number % 3 == 0 and number % 5 ==0:
     print('CracklePop')
-    continue
-   if number % 3 == 0:
+   elif number % 3 == 0:
     print('Crackle')
-    continue
-   if number % 5 == 0:
+   elif number % 5 == 0:
     print('Pop')
-    continue
-   print(number)
+   else:
+    git print(number)
